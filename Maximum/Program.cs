e3166dd207360f4c3f38f192dbbe940a2126ea07
ZH-Hanna <61236743+ZH-Hanna@users.noreply.github.com>
2020-04-04 15:23:42 +0300

    Добавьте файлы проекта.

---
+++ Maximum/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Maximum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c, d, max;
+            Console.Write("Podaj pierwszą wartość: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Podaj drugą wartość: ");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Podaj trzecią wartość: ");
+            c = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Podaj czwartą wartość: ");
+            d = Convert.ToInt32(Console.ReadLine());
+            max = a > b ? a : b;
+            max = b > c ? b : c;
+            max = c > d ? c : d;
+            
+
+
+            Console.WriteLine("Wartość maksymalna wynosi: {0}.", max);
+                    Console.ReadKey();
+                }
+            }
+        } 
